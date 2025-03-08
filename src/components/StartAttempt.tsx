eecc7764ec97b440448bbf1a59dcf9a4adb3f74c
Baseer Sharief <bsharief@udel.edu>
2025-03-07 20:56:12 -0500

    testing task 8

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): React.JSX.Element {
-    const [attempts, setAttempts] = useState<number>(4);
-    const [inProgress, setInProgress] = useState<boolean>(false);
+    let [attempts, setAttempts] = useState<number>(4);
+    let [inProgress, setInProgress] = useState<boolean>(false);
 
     function startQuiz(): void {
         if (attempts > 0) {
